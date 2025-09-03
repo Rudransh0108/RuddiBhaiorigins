c55d0c958c4038e27b2fd754b678d1a6b84a6de7
Rudransh0108 <rspandey7301@gmail.com>
2025-09-04 00:05:19 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("siri-cache").then(cache => {
+    caches.open("jarvis-cache").then(cache => {
       return cache.addAll([
         "index.html",
         "manifest.json",
